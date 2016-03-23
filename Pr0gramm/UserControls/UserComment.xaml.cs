2d37e2fa7501afbb68ee2575edb277b04e7cf708
Marco.Silipo <m.silipo@web.de>
2016-03-23 17:46:03 +0100

    Further implemented UserComment

--- Pr0gramm/UserControls/UserComment.xaml.cs
+++ Pr0gramm/UserControls/UserComment.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Pr0gramm.pr0;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -19,9 +20,22 @@ namespace Pr0gramm.UserControls
 {
     public sealed partial class UserComment : UserControl
     {
-        public UserComment()
+        private ItemInfo.Comment CommentInfo;
+        
+        public UserComment(ItemInfo.Comment comment)
         {
             this.InitializeComponent();
+            this.CommentInfo = comment;
+            this.CommentElement.Text = comment.Content;
+            this.AuthorElement.Text = comment.Author;
+            foreach (var it in comment.Children)
+            {
+                this.ChildrensStack.Children.Add(new UserComment(it));
+            }
+
+            this.isOpElement.Visibility = comment.Author == comment.Owner.Owner.User ? Visibility.Visible : Visibility.Collapsed;
+            this.AuthorRank.Fill = new pr0.User.Rank((int)comment.Mark).Color;
+            this.Created.Text = comment.Created.ToString();
         }
     }
 }
