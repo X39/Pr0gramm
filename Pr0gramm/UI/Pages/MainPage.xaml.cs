c21c6c2e1ab0e460be54c147d7c72807b0d9c333
poyopaya <poyopaya@users.noreply.github.com>
2016-03-25 17:39:39 +0100

    Changed TitleBar color to pr0_grey

--- Pr0gramm/UI/Pages/MainPage.xaml.cs
+++ Pr0gramm/UI/Pages/MainPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using Windows.UI;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -12,7 +16,22 @@ namespace Pr0gramm.UI.Pages
         public MainPage()
         {
             this.InitializeComponent();
+            this.applyTitleBarTheme();
             this.ContentFrame.Navigate(typeof(Fragments.ContentPresenter), new Fragments.ContentPresenter.ViewSource(Fragments.ContentPresenter.ViewSource.ViewType.New));
+
+
+        }
+
+        private void applyTitleBarTheme()
+        {
+            ApplicationView AppView = ApplicationView.GetForCurrentView();
+            AppView.TitleBar.ButtonInactiveBackgroundColor = (Application.Current.Resources["pr0_orange"] as SolidColorBrush).Color;
+            AppView.TitleBar.ButtonInactiveForegroundColor = Colors.White;
+            AppView.TitleBar.ButtonBackgroundColor = (Application.Current.Resources["pr0_orange"] as SolidColorBrush).Color;
+            AppView.TitleBar.ButtonHoverBackgroundColor = (Application.Current.Resources["pr0_orange"] as SolidColorBrush).Color;
+            AppView.TitleBar.ButtonPressedBackgroundColor = (Application.Current.Resources["pr0_orange"] as SolidColorBrush).Color;
+            AppView.TitleBar.BackgroundColor = (Application.Current.Resources["pr0_orange"] as SolidColorBrush).Color;
+            AppView.TitleBar.InactiveBackgroundColor = (Application.Current.Resources["pr0_orange"] as SolidColorBrush).Color;
         }
     }
 }
