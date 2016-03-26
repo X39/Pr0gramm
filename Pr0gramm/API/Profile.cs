641f223f48b3ea218740aa25537b9e135b6e58ec
Marco.Silipo <m.silipo@web.de>
2016-03-26 01:34:06 +0100

    Implemented Profile API call

---
+++ Pr0gramm/API/Profile.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using asapJson;
+using Pr0gramm.API.ProfileUtil;
+using Pr0gramm.API.Util;
+using System.Net;
+
+namespace Pr0gramm.API
+{
+    public class Profile
+    {
+        private Profile(JsonNode node)
+        {
+            JsonNode tmpNode;
+            tmpNode = node.getValue_Object()["user"];
+            this.UserID = (long)tmpNode.getValue_Object()["id"].getValue_Number();
+            this.Username = (long)tmpNode.getValue_Object()["name"].getValue_Number();
+            this.Registered = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds(tmpNode.getValue_Object()["registered"].getValue_Number());
+            this.Score = (long)tmpNode.getValue_Object()["score"].getValue_Number();
+            this.MarkObj = new API.ProfileUtil.Mark((int)tmpNode.getValue_Object()["score"].getValue_Number());
+            this.IsAdmin = tmpNode.getValue_Object()["admin"].getValue_Boolean();
+            this.IsBanned = tmpNode.getValue_Object()["banned"].getValue_Boolean();
+            this.CommentCount = (long)node.getValue_Object()["commentCount"].getValue_Number();
+            this.LikeCount = (long)node.getValue_Object()["likeCount"].getValue_Number();
+            this.TagCount = (long)node.getValue_Object()["tagCount"].getValue_Number();
+            this.FollowCount = (long)node.getValue_Object()["followCount"].getValue_Number();
+            this.Timestamp = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds(tmpNode.getValue_Object()["ts"].getValue_Number());
+            this.Following = node.getValue_Object()["following"].getValue_Boolean();
+            //this.Cache = node.getValue_Object()["cache"];
+            this.Rt = (long)node.getValue_Object()["rt"].getValue_Number();
+            this.Qc = (long)node.getValue_Object()["qc"].getValue_Number();
+
+            tmpNode = tmpNode.getValue_Object()["bannedUntil"];
+            if(tmpNode != null && tmpNode.Type != JsonNode.EJType.Object)
+                this.BannedUntil = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds(tmpNode.getValue_Number());
+
+
+            tmpNode = node.getValue_Object()["comments"];
+            this.Comments = new List<API.Util.Message>();
+            foreach (var it in tmpNode.getValue_Array())
+            {
+                this.Comments.Add(new Message(it));
+            }
+
+            tmpNode = node.getValue_Object()["uploads"];
+            this.Uploads = new List<API.Util.Image>();
+            foreach (var it in tmpNode.getValue_Array())
+            {
+                this.Uploads.Add(new API.Util.Image(it));
+            }
+
+            tmpNode = node.getValue_Object()["badges"];
+            this.Badges = new List<API.ProfileUtil.Badge>();
+            foreach (var it in tmpNode.getValue_Array())
+            {
+                this.Badges.Add(new API.ProfileUtil.Badge(it));
+            }
+        }
+
+        public List<Badge> Badges { get; private set; }
+        public DateTime? BannedUntil { get; private set; }
+        public long CommentCount { get; private set; }
+        public List<API.Util.Message> Comments { get; private set; }
+        public long FollowCount { get; private set; }
+        public bool Following { get; private set; }
+        public bool IsAdmin { get; private set; }
+        public bool IsBanned { get; private set; }
+        public long LikeCount { get; private set; }
+        public Mark MarkObj { get; private set; }
+        public long Qc { get; private set; }
+        public DateTime Registered { get; private set; }
+        public long Rt { get; private set; }
+        public long Score { get; private set; }
+        public long TagCount { get; private set; }
+        public DateTime Timestamp { get; private set; }
+        public List<API.Util.Image> Uploads { get; private set; }
+        public long UserID { get; private set; }
+        public long Username { get; private set; }
+
+        public async Task<Profile> Fetch(string profileName)
+        {
+            string url = app.Settings.Pr0grammUrl.Api;
+            url += "profile/info?name=" + WebUtility.UrlEncode(profileName);
+            WebRequest request = WebRequest.Create(url);
+            request.Method = "GET";
+            request.Credentials = CredentialCache.DefaultCredentials;
+            request.Headers["User-Agent"] = app.Settings.UserAgent;
+            ((HttpWebRequest)request).CookieContainer = app.Settings.Instance.Cookie;
+
+            var response = await request.GetResponseAsync();
+
+            asapJson.JsonNode responseNode = new asapJson.JsonNode(new System.IO.StreamReader(response.GetResponseStream()).ReadToEnd(), true);
+            response.Dispose();
+            return new Profile(responseNode);
+        }
+    }
+}
---
+++ Pr0gramm/API/ProfileUtil/Badge.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Pr0gramm.API.ProfileUtil
+{
+    public class Badge
+    {
+
+        public Badge(asapJson.JsonNode sourceNode)
+        {
+            this.Link = sourceNode.getValue_Object()["link"].getValue_String();
+            this.Image = sourceNode.getValue_Object()["image"].getValue_String();
+            this.Description = sourceNode.getValue_Object()["description"].getValue_String();
+            this.Created = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds(sourceNode.getValue_Object()["created"].getValue_Number());
+        }
+
+        public DateTime Created { get; private set; }
+        public string Description { get; private set; }
+        public string Image { get; private set; }
+        public string Link { get; private set; }
+    }
+}
