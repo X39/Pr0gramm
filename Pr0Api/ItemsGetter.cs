602d5be6d022d8f8131eba112f75f382bc24aeb6
Marco.Silipo <m.silipo@web.de>
2016-03-28 17:14:48 +0200

    Added ItemsGetter api object

---
+++ Pr0Api/ItemsGetter.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Pr0gramm.API.ItemsGetterUtil;
+using Windows.Web.Http;
+
+namespace Pr0gramm.API
+{
+    public class ItemsGetter
+    {
+
+        private ItemsGetter(asapJson.JsonNode node, ViewSource vs, UrlProvider url, Windows.Web.Http.HttpCookie cookie)
+        {
+            this.View = vs;
+            this.AtEnd = node.getValue_Object()["atEnd"].getValue_Boolean();
+            this.AtStart = node.getValue_Object()["atStart"].getValue_Boolean();
+            this.Items = new List<Image>();
+            foreach(var it in node.getValue_Object()["items"].getValue_Array())
+            {
+                this.Items.Add(new Image(it));
+            }
+            this.Timestamp = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds(node.getValue_Object()["ts"].getValue_Number());
+            this.Cache = node.getValue_Object()["cache"].getValue_String();
+            this.Rt = (long)node.getValue_Object()["rt"].getValue_Number();
+            this.Qc = (long)node.getValue_Object()["qc"].getValue_Number();
+            this.Url = url;
+            this.Cookie = cookie;
+        }
+
+        public bool AtEnd { get; private set; }
+        public bool AtStart { get; private set; }
+        public string Cache { get; private set; }
+        public List<Image> Items { get; private set; }
+        public long Qc { get; private set; }
+        public long Rt { get; private set; }
+        public DateTime Timestamp { get; private set; }
+        public ViewSource View { get; private set; }
+        public UrlProvider Url { get; private set; }
+        public HttpCookie Cookie { get; private set; }
+
+        public async Task GetNewer(Action<List<Image>> callback)
+        {
+            if (this.AtStart)
+            {
+                callback.Invoke(new List<Image>());
+                return;
+            }
+            string fetchUrl = this.Url.Api + this.View.RequestPath + "&newer=" + this.Items.First().Id;
+            var filter = new Windows.Web.Http.Filters.HttpBaseProtocolFilter();
+            if (this.Cookie != null)
+                filter.CookieManager.SetCookie(this.Cookie);
+            Windows.Web.Http.HttpClient client = new Windows.Web.Http.HttpClient(filter);
+
+            client.DefaultRequestHeaders.UserAgent.TryParseAdd(this.Url.UserAgent);
+            var response = await client.GetAsync(new Uri(fetchUrl));
+
+            asapJson.JsonNode responseNode = new asapJson.JsonNode(await response.Content.ReadAsStringAsync(), true);
+            response.Dispose();
+            this.AtStart = responseNode.getValue_Object()["atStart"].getValue_Boolean();
+            var newItems = new List<Image>();
+            foreach (var it in responseNode.getValue_Object()["items"].getValue_Array())
+            {
+                newItems.Add(new Image(it));
+            }
+            this.Items.InsertRange(0, newItems);
+            callback.Invoke(newItems);
+        }
+        public async Task GetOlder(Action<List<Image>> callback)
+        {
+            if (this.AtStart)
+            {
+                callback.Invoke(new List<Image>());
+                return;
+            }
+            string fetchUrl = this.Url.Api + this.View.RequestPath + "&older=" + this.Items.Last().Id;
+            var filter = new Windows.Web.Http.Filters.HttpBaseProtocolFilter();
+            if (this.Cookie != null)
+                filter.CookieManager.SetCookie(this.Cookie);
+            Windows.Web.Http.HttpClient client = new Windows.Web.Http.HttpClient(filter);
+
+            client.DefaultRequestHeaders.UserAgent.TryParseAdd(this.Url.UserAgent);
+            var response = await client.GetAsync(new Uri(fetchUrl));
+
+            asapJson.JsonNode responseNode = new asapJson.JsonNode(await response.Content.ReadAsStringAsync(), true);
+            response.Dispose();
+            this.AtEnd = responseNode.getValue_Object()["atEnd"].getValue_Boolean();
+            var newItems = new List<Image>();
+            foreach (var it in responseNode.getValue_Object()["items"].getValue_Array())
+            {
+                newItems.Add(new Image(it));
+            }
+            this.Items.AddRange(newItems);
+            callback.Invoke(newItems);
+        }
+
+        public static async Task<ItemsGetter> Fetch(UrlProvider url, ViewSource vs, Windows.Web.Http.HttpCookie cookie = null)
+        {
+            string fetchUrl = url.Api + vs.RequestPath;
+            var filter = new Windows.Web.Http.Filters.HttpBaseProtocolFilter();
+            if (cookie != null)
+                filter.CookieManager.SetCookie(cookie);
+            Windows.Web.Http.HttpClient client = new Windows.Web.Http.HttpClient(filter);
+
+            client.DefaultRequestHeaders.UserAgent.TryParseAdd(url.UserAgent);
+            var response = await client.GetAsync(new Uri(fetchUrl));
+
+            asapJson.JsonNode responseNode = new asapJson.JsonNode(await response.Content.ReadAsStringAsync(), true);
+            response.Dispose();
+            return new ItemsGetter(responseNode, vs, url, cookie);
+        }
+    }
+}
---
+++ Pr0Api/ItemsGetterUtil/ViewSource.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace Pr0gramm.API.ItemsGetterUtil
+{
+    public class ViewSource
+    {
+        public readonly ViewType viewType;
+        public readonly string data;
+        public readonly int filterMode;
+
+        public enum ViewType
+        {
+            New,
+            Top,
+            UserImages,
+            UserFavorites
+        }
+        public enum FilterMode
+        {
+            sfw = 1,
+            nsfw,
+            sfw_nsfw,
+            nsfl,
+            sfw_nsfl,
+            nsfw_nsfl,
+            sfw_nsfw_nsfl
+        }
+
+        public ViewSource(ViewType vt)
+        {
+            this.viewType = vt;
+            this.data = "";
+            this.filterMode = 0;
+        }
+        /// <summary>
+        /// Creates new ViewSource with given parameters
+        /// </summary>
+        /// <param name="vt">ViewType repesented by this ViewSource</param>
+        /// <param name="data">If ViewType.New or ViewType.Top then either empty or the search param else the user represented by this ViewSource</param>
+        /// <param name="mode">FilterMode represented by this ViewSource</param>
+        public ViewSource(ViewType vt, string data, FilterMode mode = FilterMode.sfw)
+        {
+            if ((vt == ViewType.UserFavorites || vt == ViewType.UserImages))
+            {
+                if (string.IsNullOrWhiteSpace(data))
+                    throw new ArgumentException("UserX ViewTypes require a username in data param");
+            }
+            this.viewType = vt;
+            this.data = data;
+            this.filterMode = (int)mode;
+        }
+        /// <summary>
+        /// Creates new ViewSource with given parameters
+        /// </summary>
+        /// <param name="vt">ViewType repesented by this ViewSource</param>
+        /// <param name="data">If ViewType.New or ViewType.Top then either empty or the search param else the user represented by this ViewSource</param>
+        /// <param name="mode">FilterMode represented by this ViewSource, range from 0 to 7</param>
+        public ViewSource(ViewType vt, string data, int mode = 0)
+        {
+            if ((vt == ViewType.UserFavorites || vt == ViewType.UserImages))
+            {
+                if (string.IsNullOrWhiteSpace(data))
+                    throw new ArgumentException("UserX ViewTypes require a username in data param");
+                if (mode < 0 || mode > 7)
+                    throw new ArgumentException("mode has to be in range 0 - 7");
+            }
+            this.viewType = vt;
+            this.data = data;
+            this.filterMode = mode;
+        }
+
+        public string RequestPath
+        {
+            get
+            {
+                switch (this.viewType)
+                {
+                    case ViewType.New:
+                        return "items/get?promoted=0" + "&flags=" + this.filterMode + (string.IsNullOrWhiteSpace(data) ? "" : "&tags=" + System.Net.WebUtility.UrlEncode(data.Replace(' ', '+')));
+                    case ViewType.Top:
+                        return "items/get?promoted=1" + "&flags=" + this.filterMode + (string.IsNullOrWhiteSpace(data) ? "" : "&tags=" + System.Net.WebUtility.UrlEncode(data.Replace(' ', '+')));
+                    case ViewType.UserFavorites:
+                        return "items/get?likes=" + WebUtility.UrlEncode(data) + "&flags=" + this.filterMode;
+                    case ViewType.UserImages:
+                        return "items/get?user=" + WebUtility.UrlEncode(data) + "&flags=" + this.filterMode;
+                    default:
+                        return "";
+                }
+            }
+        }
+    }
+}
