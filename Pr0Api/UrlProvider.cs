ae758d532a927c43e08d8b66312885adbdf7ecff
poyopaya <poyopaya@users.noreply.github.com>
2016-04-02 01:33:27 +0200

    removed unnessessary imports

--- Pr0Api/UrlProvider.cs
+++ Pr0Api/UrlProvider.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Pr0gramm.API
+﻿namespace Pr0gramm.API
 {
     public class UrlProvider
     {
