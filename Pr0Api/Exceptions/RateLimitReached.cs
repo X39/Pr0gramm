05316ec545d765e60d9be7e85a13417f85488abf
Marco.Silipo <m.silipo@web.de>
2016-04-01 22:13:03 +0200

    Added RateLimitReached exception

---
+++ Pr0Api/Exceptions/RateLimitReached.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr0gramm.API.Exceptions
+{
+    public class RateLimitReached : Exception
+    {
+        public RateLimitReached() : base("403, Rate Limit Reached")
+        {
+
+        }
+    }
+}
