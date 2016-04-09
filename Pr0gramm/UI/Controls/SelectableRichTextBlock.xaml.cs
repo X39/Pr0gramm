794fe42b0f74f736b3fe888aceee1896c608fd1c
Marco.Silipo <m.silipo@web.de>
2016-04-09 15:22:26 +0200

    Now using RichTextBlock and rewrote link detection

--- Pr0gramm/UI/Controls/SelectableRichTextBlock.xaml.cs
+++ Pr0gramm/UI/Controls/SelectableRichTextBlock.xaml.cs
@@ -1,18 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-using Windows.UI.Text;
+using Windows.UI.Xaml.Documents;
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -20,6 +10,8 @@ namespace Pr0gramm.UI.Controls
 {
     public sealed partial class SelectableRichTextBlock : UserControl
     {
+        public event EventHandler<Uri> LinkClicked;
+
         public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(string), new PropertyMetadata(default(string)));
         public string Text
         {
@@ -32,76 +24,66 @@ namespace Pr0gramm.UI.Controls
         }
         private void parseIntoComment(string content)
         {
-            //DO NOT ASK WHY ... WinRT is love :facepalm:
-            //thx to you Jason Jarrett ... http://elegantcode.com/2013/03/19/richeditbox-gives-unauthorizedaccessexception-access-is-denied-error-when-settext-called/
-            this.ContentElement.IsReadOnly = false;
-            this.ContentElement.Document.SetText(TextSetOptions.None, content);
-            //Again ... do not ask why ...
-            this.ContentElement.Document.GetText(TextGetOptions.None, out content);
+            var linkPrefixes = new string[] { "http://", "https://" };
             int index = 0;
+            int lastIndex = 0;
             do
             {
-                index = content.IndexOf("http://", index);
-                if (index < 0)
-                    break;
-                int indexEnd = content.IndexOfAny(new char[] { ' ', '\r', '\n', '\0', '\t' }, index);
-                if (indexEnd < 0)
-                    indexEnd = content.Length;
-                var range = this.ContentElement.Document.GetRange(index, indexEnd);
-                if (range.Text.StartsWith("http://pr0gramm.com"))
-                    range.Text = range.Text.Substring("http://pr0gramm.com".Length);
-                range.Link = '"' + content.Substring(index, indexEnd - index) + '"';
-                
-                index = range.EndPosition;
-                this.ContentElement.Document.GetText(TextGetOptions.None, out content);
-            } while (true);
-            index = 0;
-            do
-            {
-                index = content.IndexOf("https://", index);
-                if (index < 0)
-                    break;
-                int indexEnd = content.IndexOfAny(new char[] { ' ', '\r', '\n', '\0', '\t' }, index);
-                if (indexEnd < 0)
-                    indexEnd = content.Length;
-                var range = this.ContentElement.Document.GetRange(index, indexEnd);
-                if (range.Text.StartsWith("https://pr0gramm.com"))
-                    range.Text = range.Text.Substring("https://pr0gramm.com".Length);
-                range.Link = '"' + content.Substring(index, indexEnd - index) + '"';
-                index = range.EndPosition;
-                this.ContentElement.Document.GetText(TextGetOptions.None, out content);
-            } while (true);
-            this.ContentElement.Document.ApplyDisplayUpdates();
-            //*sigh* ...
-            this.ContentElement.IsReadOnly = true;
-        }
-
-        public event EventHandler<string> LinkClicked;
-        private void ContentElement_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-            if (LinkClicked != null)
-            {
-                var p = e.GetPosition((UIElement)sender);
-                var range = this.ContentElement.Document.GetRangeFromPoint(p, PointOptions.ClientCoordinates);
-                range.StartOf(TextRangeUnit.Link, true);
-                if(!string.IsNullOrWhiteSpace(range.Link))
+                index = content.IndexOf(linkPrefixes[0], lastIndex);
+                int index2 = content.IndexOf(linkPrefixes[1], lastIndex);
+                string currentPrefix;
+                if ((index2 < index || index == -1) && index2 != -1)
                 {
-                    LinkClicked(this, range.Link);
+                    index = index2;
+                    currentPrefix = linkPrefixes[1];
                 }
-            }
-        }
+                else
+                {
+                    currentPrefix = linkPrefixes[0];
+                }
+                if (index < 0)
+                {
+                    Span span = new Span();
+                    span.Inlines.Add(new Run { Text = content.Substring(lastIndex) });
+                    Paragraph p = new Paragraph();
+                    p.Inlines.Add(span);
+                    this.ContentElement.Blocks.Add(p);
+                    break;
+                }
+                else if (index > 0)
+                {
+                    Span span = new Span();
+                    span.Inlines.Add(new Run { Text = content.Substring(lastIndex, index) });
 
-        private void ContentElement_PointerPressed(object sender, PointerRoutedEventArgs e)
-        {
-            var p = e.GetCurrentPoint((UIElement)sender);
-            var range = this.ContentElement.Document.GetRangeFromPoint(p.Position, PointOptions.ClientCoordinates);
-            range.StartOf(TextRangeUnit.Link, true);
-            if (!string.IsNullOrWhiteSpace(range.Link))
-            {
-                if(this.LinkClicked != null)
-                    this.LinkClicked(this, range.Link);
-                e.Handled = true;
-            }
+                    Paragraph p = new Paragraph();
+                    p.Inlines.Add(span);
+                    this.ContentElement.Blocks.Add(p);
+                }
+                int indexEnd = content.IndexOfAny(new char[] { ' ', '\r', '\n', '\0', '\t' }, index);
+                if (indexEnd < 0)
+                    indexEnd = content.Length;
+
+                string link = content.Substring(index, indexEnd - index);
+                string linkText = link.StartsWith(currentPrefix + "pr0gramm.com") ? link.Substring(currentPrefix.Length + "pr0gramm.com".Length) : link;
+
+                Hyperlink lnk = new Hyperlink();
+                lnk.Inlines.Add(new Run { Text = linkText });
+                lnk.NavigateUri = new Uri(link);
+                lnk.Foreground = Application.Current.Resources["pr0_orange"] as SolidColorBrush;
+                lnk.Click += (sender, e) =>
+                {
+                    if (this.LinkClicked != null)
+                    {
+                        this.LinkClicked(lnk, lnk.NavigateUri);
+                    }
+                };
+                {
+                    Paragraph p = new Paragraph();
+                    p.Inlines.Add(lnk);
+                    this.ContentElement.Blocks.Add(p);
+                    lastIndex = indexEnd;
+                }
+            } while (true);
         }
     }
 }
