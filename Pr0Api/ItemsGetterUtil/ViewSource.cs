43bf1ecdff512a3b7f1e32b2e1515f469a2735d4
Marco.Silipo <m.silipo@web.de>
2016-04-14 02:45:29 +0200

    Added a way to get view around specified ID

--- Pr0Api/ItemsGetterUtil/ViewSource.cs
+++ Pr0Api/ItemsGetterUtil/ViewSource.cs
@@ -8,6 +8,7 @@ namespace Pr0gramm.API.ItemsGetterUtil
         public readonly ViewType viewType;
         public readonly string data;
         public readonly int filterMode;
+        public readonly int imageID;
 
         public enum ViewType
         {
@@ -56,7 +57,8 @@ namespace Pr0gramm.API.ItemsGetterUtil
         /// <param name="vt">ViewType repesented by this ViewSource</param>
         /// <param name="data">If ViewType.New or ViewType.Top then either empty or the search param else the user represented by this ViewSource</param>
         /// <param name="mode">FilterMode represented by this ViewSource, range from 0 to 7</param>
-        public ViewSource(ViewType vt, string data, int mode = 0)
+        /// <param name="imgId">Image id to start with, leave 0 if you do not need</param>
+        public ViewSource(ViewType vt, string data, int mode = 0, int imgId = 0)
         {
             if ((vt == ViewType.UserFavorites || vt == ViewType.UserImages))
             {
@@ -65,6 +67,8 @@ namespace Pr0gramm.API.ItemsGetterUtil
                 if (mode < 0 || mode > 7)
                     throw new ArgumentException("mode has to be in range 0 - 7");
             }
+            if(imgId < 0)
+                throw new ArgumentException("imgId has to be > 0");
             this.viewType = vt;
             this.data = data;
             this.filterMode = mode;
@@ -77,13 +81,13 @@ namespace Pr0gramm.API.ItemsGetterUtil
                 switch (this.viewType)
                 {
                     case ViewType.New:
-                        return "items/get?promoted=0" + "&flags=" + this.filterMode + (string.IsNullOrWhiteSpace(data) ? "" : "&tags=" + System.Net.WebUtility.UrlEncode(data.Replace(' ', '+')));
+                        return "items/get?promoted=0" + "&flags=" + this.filterMode + (string.IsNullOrWhiteSpace(data) ? "" : "&tags=" + WebUtility.UrlEncode(data.Replace(' ', '+'))) + (imageID > 0 ? "&id=" + WebUtility.UrlEncode(imageID.ToString()) : "");
                     case ViewType.Top:
-                        return "items/get?promoted=1" + "&flags=" + this.filterMode + (string.IsNullOrWhiteSpace(data) ? "" : "&tags=" + System.Net.WebUtility.UrlEncode(data.Replace(' ', '+')));
+                        return "items/get?promoted=1" + "&flags=" + this.filterMode + (string.IsNullOrWhiteSpace(data) ? "" : "&tags=" + WebUtility.UrlEncode(data.Replace(' ', '+')) + (imageID > 0 ? "&id=" + WebUtility.UrlEncode(imageID.ToString()) : ""));
                     case ViewType.UserFavorites:
-                        return "items/get?likes=" + WebUtility.UrlEncode(data) + "&flags=" + this.filterMode;
+                        return "items/get?likes=" + WebUtility.UrlEncode(data) + "&flags=" + this.filterMode + (imageID > 0 ? "&id=" + WebUtility.UrlEncode(imageID.ToString()) : "");
                     case ViewType.UserImages:
-                        return "items/get?user=" + WebUtility.UrlEncode(data) + "&flags=" + this.filterMode;
+                        return "items/get?user=" + WebUtility.UrlEncode(data) + "&flags=" + this.filterMode + (imageID > 0 ? "&id=" + WebUtility.UrlEncode(imageID.ToString()) : "");
                     default:
                         return "";
                 }
