aa80d033c741211b818710213520a00edd245581
Marco.Silipo <m.silipo@web.de>
2016-04-14 02:45:49 +0200

    Changed tuple return to object return & implemented

--- Pr0gramm/UI/Controls/SelectableRichTextBlock.xaml.cs
+++ Pr0gramm/UI/Controls/SelectableRichTextBlock.xaml.cs
@@ -11,6 +11,21 @@ namespace Pr0gramm.UI.Controls
 {
     public sealed partial class SelectableRichTextBlock : UserControl
     {
+        public static readonly char[] HandleTerminateCharacters = { ' ', '\r', '\n', '\0', '\t' };
+        public class CustomHandlersReturn
+        {
+            internal int StartIndex { get; private set; }
+            internal int EndIndex { get; private set; }
+            internal string Link { get; private set; }
+            internal string Text { get; private set; }
+            public CustomHandlersReturn(int startIndex, int endIndex = -1, string link = default(string), string linkText = default(string))
+            {
+                this.StartIndex = startIndex;
+                this.EndIndex = endIndex;
+                this.Link = link;
+                this.Text = string.IsNullOrWhiteSpace(linkText) ? link : linkText;
+            }
+        }
         public class LinkClickedEventArgs : EventArgs
         {
             public bool Handled { get; set; }
@@ -30,11 +45,12 @@ namespace Pr0gramm.UI.Controls
             get { return (string)GetValue(TextProperty); }
             set { SetValue(TextProperty, value); parseIntoComment(value); }
         }
-        public List<Func<string, int, Tuple<int, string, string>>> CustomHandlers { get; private set; }
+
+        public List<Func<string, int, CustomHandlersReturn>> CustomHandlers { get; private set; }
         public SelectableRichTextBlock()
         {
             HyperlinkRegister = new Dictionary<Hyperlink, Uri>();
-            CustomHandlers = new List<Func<string, int, Tuple<int, string, string>>>();
+            CustomHandlers = new List<Func<string, int, CustomHandlersReturn>>();
             this.InitializeComponent();
         }
         private void parseIntoComment(string content)
@@ -46,7 +62,32 @@ namespace Pr0gramm.UI.Controls
             {
                 foreach(var fnc in this.CustomHandlers)
                 {
-                    fnc.Invoke(content, lastIndex);
+                    var result = fnc.Invoke(content, lastIndex);
+                    if (result.StartIndex > 0)
+                    {
+                        Hyperlink lnk = new Hyperlink();
+                        lnk.Inlines.Add(new Run { Text = result.Text });
+                        HyperlinkRegister[lnk] = new Uri(result.Link);
+                        lnk.Foreground = Application.Current.Resources["pr0_orange"] as SolidColorBrush;
+                        lnk.Click += (sender, e) =>
+                        {
+                            sender.NavigateUri = null;
+                            var uri = HyperlinkRegister[sender];
+                            var args = new LinkClickedEventArgs(uri);
+                            if (this.LinkClicked != null)
+                            {
+                                this.LinkClicked(sender, args);
+                            }
+                            if (!args.Handled)
+                            {
+                                sender.NavigateUri = uri;
+                            }
+                        };
+                        Paragraph p = new Paragraph();
+                        p.Inlines.Add(lnk);
+                        this.ContentElement.Blocks.Add(p);
+                        lastIndex = index = result.EndIndex;
+                    }
                 }
                 index = content.IndexOf(linkPrefixes[0], lastIndex);
                 int index2 = content.IndexOf(linkPrefixes[1], lastIndex);
@@ -81,32 +122,32 @@ namespace Pr0gramm.UI.Controls
                     p.Inlines.Add(span);
                     this.ContentElement.Blocks.Add(p);
                 }
-                int indexEnd = content.IndexOfAny(new char[] { ' ', '\r', '\n', '\0', '\t' }, index);
+                int indexEnd = content.IndexOfAny(HandleTerminateCharacters, index);
                 if (indexEnd < 0)
                     indexEnd = content.Length;
 
                 string link = content.Substring(index, indexEnd - index);
                 string linkText = link.StartsWith(currentPrefix + "pr0gramm.com") ? link.Substring(currentPrefix.Length + "pr0gramm.com".Length) : link;
 
-                Hyperlink lnk = new Hyperlink();
-                lnk.Inlines.Add(new Run { Text = linkText });
-                HyperlinkRegister[lnk] = new Uri(link);
-                lnk.Foreground = Application.Current.Resources["pr0_orange"] as SolidColorBrush;
-                lnk.Click += (sender, e) =>
                 {
-                    sender.NavigateUri = null;
-                    var uri = HyperlinkRegister[sender];
-                    var args = new LinkClickedEventArgs(uri);
-                    if (this.LinkClicked != null)
+                    Hyperlink lnk = new Hyperlink();
+                    lnk.Inlines.Add(new Run { Text = linkText });
+                    HyperlinkRegister[lnk] = new Uri(link);
+                    lnk.Foreground = Application.Current.Resources["pr0_orange"] as SolidColorBrush;
+                    lnk.Click += (sender, e) =>
                     {
-                        this.LinkClicked(sender, args);
-                    }
-                    if (!args.Handled)
-                    {
-                        sender.NavigateUri = uri;
-                    }
-                };
-                {
+                        sender.NavigateUri = null;
+                        var uri = HyperlinkRegister[sender];
+                        var args = new LinkClickedEventArgs(uri);
+                        if (this.LinkClicked != null)
+                        {
+                            this.LinkClicked(sender, args);
+                        }
+                        if (!args.Handled)
+                        {
+                            sender.NavigateUri = uri;
+                        }
+                    };
                     Paragraph p = new Paragraph();
                     p.Inlines.Add(lnk);
                     this.ContentElement.Blocks.Add(p);
